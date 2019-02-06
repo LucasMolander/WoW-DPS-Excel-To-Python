01a02cbe004661fa529892cad17e0aea2c1fade2
Lucas Molander <lmmoland@ncsu.edu>
2019-02-05 22:07:53 -0800

    Added a spreadsheet and some boilerplate empty-ish files

---
+++ main.py
@@ -0,0 +1,11 @@
+#
+# The main entry point for the program.
+#
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,7 @@
+#
+# For rapid tests of new stuff.
+#
+
+import formulas
+
+# Do something with formulas...
